dd1d8ef82bb4378417e586ff8348f6948cc5cf86
Hooda Armaan <hooda.armaan@syk.fi>
2022-08-28 12:35:08 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean shining = true;
+        int time = 16;
+        if (shining==true&&((time>=10)&&(time<=16)))
+        {
+            System.out.println("Please use sunscreen.");
+        }
+    }
+}
